846f6d8b738c54e6b02fe032985dbe7baad74c04
Hyuto <wahyusetianto19@gmail.com>
2021-05-10 14:48:20 +0700

    Set up whitenose

--- rock_paper/wsgi.py
+++ rock_paper/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rock_paper.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
