7a5de7d33bf983c96fb8401c65d04faf51340dde
Hyuto <wahyusetianto19@gmail.com>
2021-05-16 04:49:15 +0700

    Moving the app to react app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import { start } from "./scripts/script";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.inputRef = {
+      'canvas' : useRef(null),
+      'video' : useRef(null),
+      'prediction' : useRef(null)
+    }
+  }
+
+  render() {
+    /* const script = new RockPaperScissor(
+      this.inputRef['video'].current,
+      this.inputRef['canvas'].current,
+      this.inputRef['prediction'].current
+    ) */
+
+    return (
+        <div className="App container">
+            <div className="container">
+                <video ref={this.inputRef['video']} autoPlay={true} width="128" height="128"></video>
+                <figure className="figure">
+                    <canvas ref={this.inputRef['canvas']} className="figure-img img-fluid rounded" width="128" height="128"></canvas>
+                    <figcaption ref={this.inputRef['prediction']}  className="figure-caption text-center"></figcaption>
+                </figure>
+            </div>
+            <div className="container btn-wrapper">
+                <button className="btn btn-dark" onClick={start(this.inputRef['video'].current)}>Start</button>
+                <button className="btn btn-dark">Stop</button>
+                <button className="btn btn-dark">Take and Predict</button>
+            </div>
+        </div>
+    )
+  }
 }
 
 export default App;
