353c2f74369e3d30a42297876c1aafdc69c5cb78
Hyuto <wahyusetianto19@gmail.com>
2021-05-17 02:26:20 +0700

    Setup staticfiles folder for deploy on heroku

--- rock_paper/settings.py
+++ rock_paper/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'build' / 'static'
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
